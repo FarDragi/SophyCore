1d8dc0755e1dfefcea881603d3b1c4926bce8538
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-06-04 21:24:53 -0300

    Add: get or crate database user

--- src/database/functions/user.rs
+++ src/database/functions/user.rs
@@ -8,7 +8,8 @@ use crate::{
 #[async_trait]
 pub trait UserRepository {
     async fn exist_user(&self, id: u64) -> Result<bool, AppError>;
-    async fn create_user(&self, id: u64) -> Result<(), AppError>;
+    async fn create_user(&self, id: u64) -> Result<user::Model, AppError>;
+    async fn get_or_create_user(&self, id: u64) -> Result<user::Model, AppError>;
 }
 
 #[async_trait]
@@ -22,14 +23,25 @@ impl UserRepository for Database {
         Ok(user_exist > 0)
     }
 
-    async fn create_user(&self, id: u64) -> Result<(), AppError> {
+    async fn create_user(&self, id: u64) -> Result<user::Model, AppError> {
         let user = user::ActiveModel {
             id: Set(id.to_string()),
             ..Default::default()
         };
 
-        user.insert(&self.connection).await.map_app_err()?;
+        user.insert(&self.connection).await.map_app_err()
+    }
 
-        Ok(())
+    async fn get_or_create_user(&self, id: u64) -> Result<user::Model, AppError> {
+        let user = user::Entity::find_by_id(id.to_string())
+            .one(&self.connection)
+            .await
+            .map_app_err()?;
+
+        if let Some(user) = user {
+            Ok(user)
+        } else {
+            self.create_user(id).await
+        }
     }
 }
